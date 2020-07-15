a90e91eaa56865f0704c792dcd0b75194202500b
Conor Higgins <conor.higgins@teamwork.com>
2020-07-15 04:03:01 +0100

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git add -A
 git commit -m 'deploy'
 
 # # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:ConorHiggins/conorhiggins.github.io.git master
+git push -f git@github.com:ConorHiggins/matching-game.git master
 
 cd -
